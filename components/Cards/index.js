f42cdf7fc69b7df9ec4388b57970403510a31b55
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-05-08 11:30:03 -0500

    cards pseudo code

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,17 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+//axios
+
+//response.data.articles  within here, we have 5 arrays filled with objects. Each array corresponds with a topic.
+
+//articles is itself an object. we've got bootstrap, javascript, jquery, node, technology as arrays with arrays inside
+
+//i think we'll be doing something like this? response.data.articles.bootstrap, as an example. Bootstrap is an array with 3 articles, each article object has a headline, authorPhoto and authorName
+
+//should have a total of 15 articles
+
+//probably want to do Object.entries. so first const articleEntries = Object.entries(articles); and then we can do articleEntries.forEach
+
+//deconstruct Article Object using Object.entries and  .forEach method on its values, which happen to be the arrays that were matched with topic keys.
