8d11ffb66e32a1f6ad2d906d1ddb777e6a776015
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-05-08 13:02:22 -0500

    started stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,44 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const carouselGroup = document.querySelector(".carousel.container");
+
+carouselGroup.appendChild(createCarousel());
+
+function createCarousel() {
+  //the carousel div
+  //add carousel class
+  //div for left button
+  //add class for left button
+  //imgs 1-4
+  //div for right button
+  //div class right button
+  const carouselDiv = document.createElement("div");
+  carouselDiv.classList.add = "carousel";
+
+  const leftBtnDiv = document.createElement("div");
+  leftBtnDiv.classList.add = "left-button";
+
+  const img1 = document.createElement("img");
+  img1.src = "./assets/carousel/mountains.jpeg";
+
+  const img2 = document.createElement("img");
+  img2.src = "./assets/carousel/computer.jpeg";
+
+  const img3 = document.createElement("img");
+  img3.src = "./assets/carousel/trees.jpeg";
+
+  const img4 = document.createElement("img");
+  img4.src = "./assets/carousel/turntable.jpeg";
+
+  const rightBtnDiv = document.createElement("div");
+  rightBtnDiv.classList.add = "right-button";
+
+  carouselDiv.appendChild(leftBtnDiv);
+  carouselDiv.appendChild(img1);
+  carouselDiv.appendChild(img2);
+  carouselDiv.appendChild(img3);
+  carouselDiv.appendChild(img4);
+  carouselDiv.appendChild(rightBtnDiv);
+}
